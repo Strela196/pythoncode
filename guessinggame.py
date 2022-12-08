55ed7c637589fd41b96923bbce626cf365fb5a91
Strela196 <119959434+Strela196@users.noreply.github.com>
2022-12-08 17:21:24 +0100

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -5,7 +5,7 @@ b = None  # b is end number
 print('''
 Hello!
 This is a game in which you try to guess a number which a computer randumly choses 
-First you will need to enter to numbers and than the computer will chose a number between thos two numbers ''')
+First you will need to enter to numbers and than the computer will chose a number between those two numbers ''')
 while True:
     try:
         a = int(input('Here you\'ll enter the first number:  '))
@@ -30,16 +30,21 @@ c = None  # its set to none so that we can later change it
 
 c = int(input('please enter a number that you think is right:  '))
 
+
 while c != startnum:
     try:
         if c < startnum + 10 and c > startnum:
             c = int(
-                input('you guesssed close but it wasnt right please try agian:  '))
+                input('you guesssed a number that is greater and close to the right number but it wasnt right please try agian:  '))
         elif c > startnum - 10 and c < startnum:
             c = int(
-                input('you guesssed close but it wasnt right please try agian:  '))
+                input('you guesssed a number that is lower and close to the right number but it wasnt right please try agian:  '))
+        elif c > startnum:
+            c = int(input(
+                'you have guessed a numbere greater than the right number please enter a new number:  '))
         else:
-            c = int(input('you guessed the wrong number please try agian:  '))
+            c = int(
+                input('you guessed a number lower than the right number please try agian:  '))
     except ValueError or IndexError:
         print('Please enter a number')
         continue
